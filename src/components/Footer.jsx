dc764080a8839589fc9307a7cf38aaf29c8c51c8
Sayyam Malik <sayyammalik738@gmail.com>
2025-12-18 03:48:33 -0800

    Update website title

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <section>
             <div className='w-full mt-16 h-96 bg-black ' >
-                <h1 className='text-white font-bold text-2xl px-9 text-center tracking-normal border-b-2 py-4'>APNA STORE</h1>
+                <h1 className='text-white font-bold text-2xl px-9 text-center tracking-normal border-b-2 py-4'>SADEEQ STORE</h1>
                 <div className=' '>
                     <div className=' text-left  text-white mt-10 px-9 '>
                         <h1 className='mt-5'>Call: <strong>03415394745</strong></h1>
